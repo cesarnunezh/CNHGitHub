e70c7d354735f9ccb31f8af1e512bc6707c80d52
César Núñez <75745356+cesarnunezh@users.noreply.github.com>
2024-03-27 11:20:23 -0500

    260324

--- Data Perú/SIAF/main.py
+++ Data Perú/SIAF/main.py
@@ -1,31 +1,43 @@
 import pandas as pd
 import requests
 import json  # Importa el módulo json
+import time
 
 # Define la URL de la API
-url = "https://api.datosabiertos.mef.gob.pe/DatosAbiertos/v1/datastore_search?"
+url = "https://api.datosabiertos.mef.gob.pe/DatosAbiertos/v1/datastore_search"
 
-# Define los parámetros de consulta para limitar los resultados a 10 registros
-params = {
-    "resource_id": "69abf535-ff9a-4975-ad13-72da442a0c44",
-    "filters": json.dumps({"RUBRO": "18",
-                           "TIPO_RECURSO": "P",  # H es canon minero, P es regalías mineras
-                           "SEC_EJEC": "300622"})
-    # "limit": 10
-}
+progList = ['138', '142', '146', '147', '148', '150', '1002', '1001']
+#'24', '30', '31', '36', '49', '51', '58', '66', '68', '73', '79', '82', '87', '90', '93', '96', '97', '101', '103', '106', '109', '113', '115', '116', '117', '122', '124', '125', '127', '129',
+dfFinal = pd.DataFrame()
 
-# Realiza la solicitud GET a la API con los parámetros de consulta
-response = requests.get(url, params=params)
+for prog in progList:
+    try:
+        # Parámetros de la consulta
+        params = {
+            "resource_id": "c28a4a61-8813-414c-ab72-44fd888292d4",
+            "filters": json.dumps({
+                "PROGRAMA_PPTO": prog
+            })
+        }
 
-# Verifica si la solicitud fue exitosa (código de respuesta 200)
-if response.status_code == 200:
-    # Imprime la respuesta de la API en formato JSON
-    data = response.json()
-    records = data['records']
-    df = pd.DataFrame.from_records(records)
-    ruta_archivo = "C:\\Users\\USER\\Downloads\\resultado.xlsx"
-    df.to_excel(ruta_archivo, index=False)
-    print(f"DataFrame exportado a {ruta_archivo} exitosamente.")
-    # print(df)
-else:
-    print("La solicitud no fue exitosa. Código de respuesta:", response.status_code)
+        # Realiza la solicitud GET a la API con los parámetros de consulta
+        response = requests.get(url, params=params)
+
+        # Verifica si la solicitud fue exitosa (código de respuesta 200)
+        if response.status_code == 200:
+            # Imprime la respuesta de la API en formato JSON
+            data = response.json()
+            records = data['records']
+            df = pd.DataFrame.from_records(records)
+            df = df.loc[:, ['PROGRAMA_PPTO', 'PROGRAMA_PPTO_NOMBRE', 'PRODUCTO_PROYECTO_NOMBRE','PRODUCTO_PROYECTO','ACTIVIDAD_ACCION_OBRA_NOMBRE','ACTIVIDAD_ACCION_OBRA', 'META_NOMBRE', 'FINALIDAD', 'TIPO_ACT_PROY']]
+            df = df.drop_duplicates()
+            df = df[(df['PRODUCTO_PROYECTO_NOMBRE']!='ACCIONES COMUNES') & (df['TIPO_ACT_PROY']=='ACTIVIDAD')]
+            dfFinal = pd.concat([dfFinal,df])
+            print("Sabes")
+
+        else:
+            print("Error al realizar la consulta. Código de estado:", response.status_code)
+
+    except:
+        print(f'No funciona el {prog}')
+        continue
