78603a1e7ab6d8bf2ddd302cade5c76c7bb77b44
César Núñez <75745356+cesarnunezh@users.noreply.github.com>
2025-01-12 19:55:46 -0600

    j

--- Personal/app.R
+++ Personal/app.R
@@ -1,10 +1,11 @@
-if (!require(shiny)) {
-  install.packages("shiny")
-}
+install.packages("shiny")
+install.packages("ggplot2")
+install.packages("dplyr")
 
 library(tidyverse)
 library(openxlsx)
 library(shiny)
+library(shinyjs)
 library(shinyWidgets)
 library(shinythemes)
 library(shinycssloaders)
@@ -15,10 +16,12 @@ library(rsconnect)
 
 #1. Importación y limpieza de base de datos ----
 
-dir <- 'D:/1. Documentos/0. Bases de datos/00. GitHub/CNH_rep/Personal'
-setwd(dir)
+#dir <- 'D:/1. Documentos/0. Bases de datos/00. GitHub/CNH_rep/Personal'
+#setwd(dir)
 
-df <- read.xlsx('ReporteVentas.xlsx', startRow = 9) 
+#df <- read.xlsx('ReporteVentas.xlsx', startRow = 9) 
+
+df <- read.xlsx("https://github.com/cesarnunezh/CNH_rep/raw/refs/heads/main/Personal/ReporteVentas.xlsx", startRow = 9)
 
 names(df) <- c("idVenta", "nDocumento", "fecha", "nombreCliente" , "dirCliente", "idCliente", "emailCliente",
                "moneda", "tipoCambio", "igv", "montoTotal", "descTotal", "estado", "vendedor" , "observaciones",
@@ -217,85 +220,132 @@ lista_mes <-df %>%
 # La UI se define utilizando la funci??n ui.R y contiene los componentes gr??ficos, 
 # como men??s, botones, gr??ficos y tablas.
 
-
 ui <- fluidPage(
+  theme = bs_theme(
+    bg = "white",    # Background color
+    fg = "black",    # Foreground (text) color
+    primary = "#54B6B5",
+    secondary = "#E8BB82",
+    success = "#031B6A",
+    base_font = font_google("Inter"),
+    code_font = font_google("JetBrains Mono"),
+    font_scale = 1.5
+  ),
+  tags$head(
+    tags$style(HTML("
+      /* Custom Navbar Colors */
+      .navbar {
+        background-color: #54B6B5 !important; /* Force Navbar background color */
+        color: #FFFFFF !important; /* Force Navbar text color */
+      }
+      .navbar .navbar-nav > li > a {
+        background-color: #54B6B5 
+        color: #FFFFFF !important; /* Force Navbar link color */
+      }
+      .navbar .navbar-brand {
+        background-color: #54B6B5 
+        color: #FFFFFF !important; /* Force Navbar brand color */
+      }
+      .navbar .navbar-nav > li > a:hover,
+      .navbar .navbar-nav > li > a:focus {
+        background-color: #54B6B5 !important; /* Navbar link hover color */
+      }
+    "))
+  ),
   navbarPage("Dashboard de La Casa de Sammy", theme = shinytheme("lumen"),
              tabPanel("Ingresos", fluid = TRUE, icon = icon("dog"),
-                      sidebarLayout(
-                        sidebarPanel(
-                          selectInput(inputId = "Year",
-                                      label = "Año",
-                                      choices = lista_anios,
-                                      selected = year(now()),
-                                      width = "220px"),
-                          selectInput(inputId = "Mes",
-                                      label = "Mes",
-                                      choices = lista_mes,
-                                      selected = month(now()),
-                                      width = "220px"),
-                          selectInput(inputId = "lineaNegocio",
-                                      label = "Línea de Negocio",
-                                      choices = lista,
-                                      selected = "Total",
-                                      width = "220px")
-                        ),
-                        mainPanel(
-                          layout_columns(
-                            value_box(
-                              textOutput("dato1"),
-                              title = "Total ventas",
-                              showcase = bs_icon("coin", size = 100),
-                              showcase_layout = "left center",
-                              theme_color = "info"),
-                            value_box(
-                              textOutput("dato2"),
-                              title = "Número de clientes",
-                              showcase = bs_icon("people", size = 100),
-                              showcase_layout = "left center",
-                              theme_color = "info"),
-                            value_box(
-                              textOutput("dato3"),
-                              title = "Nuevos clientes",
-                              showcase = bs_icon("bar-chart-fill", size = 100),
-                              showcase_layout = "left center",
-                              theme_color = "info")
-                          ),
-                          hr(),
-                          layout_columns(
-                            value_box(
-                              textOutput("dato4"),
-                              title = "Ticket promedio",
-                              showcase = bs_icon("ticket", size = 100),
-                              showcase_layout = "left center",
-                              theme_color = "info"),
-                            value_box(
-                              textOutput("dato5"),
-                              title = "Ticket promedio Mixto",
-                              showcase = bs_icon("ticket", size = 100),
-                              showcase_layout = "left center",
-                              theme_color = "info"),
-                            value_box(
-                              textOutput("dato6"),
-                              title = "Ticket máximo",
-                              showcase = bs_icon("wallet", size = 100),
-                              showcase_layout = "left center",
-                              theme_color = "info")
-                          ),
-                          hr(),
-                          layout_columns(
-                            card(
-                              full_screen = TRUE,
-                              max_height = 350,
-                              card_header("Evolución mensual"),
-                              plotOutput("graph1")
-                            ),
-                            card(
-                              full_screen = TRUE,
-                              max_height = 350,
-                              card_header("Evolución diaria"),
-                              plotOutput("graph2")
-                            )
-                          )),
+                      fluidRow(
+                        column(offset = 2,
+                               width = 3,
+                               selectInput(inputId = "Year",
+                                           label = "Año",
+                                           choices = lista_anios,
+                                           selected = year(now()),
+                                           width = "220px")),
+                        column(width = 3,
+                               selectInput(inputId = "Mes",
+                                           label = "Mes",
+                                           choices = lista_mes,
+                                           selected = month(now()),
+                                           width = "220px")),
+                        column(width = 3,
+                               selectInput(inputId = "lineaNegocio",
+                                           label = "Línea de Negocio",
+                                           choices = lista,
+                                           selected = "Total",
+                                           width = "220px"))),
+                      fluidRow(
+                        column(offset = 2,
+                               width = 12,
+                               mainPanel(
+                                 layout_columns(
+                                   value_box(
+                                     textOutput("dato1"),
+                                     title = "Total ventas",
+                                     showcase = bs_icon("coin", size = 100, style = "fill: white;"),
+                                     showcase_layout = "left center",
+                                     style = "background-color: #54B6B5; color: white; font-size: 2vw;"),
+                                   value_box(
+                                     textOutput("dato2"),
+                                     title = "Número de clientes",
+                                     showcase = bs_icon("people", size = 100),
+                                     showcase_layout = "left center",
+                                     style = "background-color: #54B6B5; color: white;"),
+                                   value_box(
+                                     textOutput("dato3"),
+                                     title = "Nuevos clientes",
+                                     showcase = bs_icon("bar-chart-fill", size = 100),
+                                     showcase_layout = "left center",
+                                     style = "background-color: #54B6B5; color: white;")
+                                 ),
+                                 hr(),
+                                 layout_columns(
+                                   value_box(
+                                     textOutput("dato4"),
+                                     title = "Ticket promedio",
+                                     showcase = bs_icon("ticket", size = 100),
+                                     showcase_layout = "left center",
+                                     style = "background-color: #54B6B5; color: white;"),
+                                   value_box(
+                                     textOutput("dato5"),
+                                     title = "Ticket promedio Mixto",
+                                     showcase = bs_icon("ticket", size = 100),
+                                     showcase_layout = "left center",
+                                     style = "background-color: #54B6B5; color: white;"),
+                                   value_box(
+                                     textOutput("dato6"),
+                                     title = "Ticket máximo",
+                                     showcase = bs_icon("wallet", size = 100),
+                                     showcase_layout = "left center",
+                                     style = "background-color: #54B6B5; color: white;")
+                                 ),
+                                 hr(),
+                                 layout_columns(
+                                   card(
+                                     full_screen = TRUE,
+                                     max_height = 350,
+                                     card_header("Evolución mensual"),
+                                     useShinyjs(),
+                                     plotOutput("graph1")
+                                     ),
+                                   card(
+                                     full_screen = TRUE,
+                                     max_height = 350,
+                                     card_header("Evolución diaria"),
+                                     plotOutput("graph2")
+                                   )
+                                   ),
+                                 hr(),
+                                 layout_columns(
+                                   card(
+                                     full_screen = TRUE,
+                                     max_height = 350,
+                                     card_header("Top productos y servicios"),
+                                     plotOutput("graph3")
+                                   )
+                                 )
+                                 )
+                               )
                         )
                       )
              ))
@@ -312,33 +362,30 @@ server <- function(input, output) {
     # Tabla diaria que divide las ventas por linea de negocio
     
     nuevasLineas  <- df %>%
-      group_by(lineaNegocio, anio, mes,dia) %>% 
-      summarise(montoDiario=sum(as.numeric(totalItem))) %>% 
-      filter(dia <= day(now())) %>% 
-      group_by(lineaNegocio,anio,mes) %>% 
-      summarise(montoMensual=sum(as.numeric(montoDiario))) %>% 
+      group_by(lineaNegocio, anio, mes) %>% 
+      summarise(montoMensual=sum(as.numeric(totalItem))) %>% 
       group_by(anio, mes) %>%
       summarise(montoMensual = sum(montoMensual), .groups = 'drop') %>% 
       mutate(lineaNegocio = "Total")
     
     
     df %>%
-      group_by(lineaNegocio, anio, mes,dia) %>% 
-      summarise(montoDiario=sum(as.numeric(totalItem))) %>% 
-      filter(dia <= day(now())) %>% 
-      group_by(lineaNegocio,anio,mes) %>% 
-      summarise(montoMensual=sum(as.numeric(montoDiario))) %>% 
+      group_by(lineaNegocio, anio, mes) %>% 
+      summarise(montoMensual=sum(as.numeric(totalItem))) %>% 
       bind_rows(nuevasLineas) %>%  
+      mutate(mes = as.Date(paste0(anio, "-", mes, "-01"))) %>%  
       ggplot() +
-      aes(x = mes, y = montoMensual, color = lineaNegocio) +
-      stat_summary(aes(y=montoMensual), fun ="mean", geom="point") +
-      stat_summary(aes(y=montoMensual), fun ="mean", geom="line") +
+      aes(x = mes, y = montoMensual, fill = lineaNegocio) +
+      geom_bar(stat = "identity", position = "dodge") +
+      geom_smooth(aes(color = lineaNegocio), method = "lm", se = FALSE, linewidth = 0.5) + 
       labs(x = "Mes",
            y = "Monto (en soles)") +
-      labs(title = "Evolución de ventas mensuales a la fecha", 
-           color = "Línea de negocio") +
+      labs(title = "Evolución de ventas mensuales 2024", 
+           color = "Línea de negocio",
+           fill = "Línea de negocio") +
+      scale_x_date(date_labels = "%m-%y", date_breaks = "1 month") +
       scale_fill_manual(values = c("Pet shop" = "#54B6B5", "Pet spa" = "#E8BB82", "Total" = "#031B6A")) +
-      theme(axis.text.x = element_text(angle = 0, hjust = 1))
+      scale_color_manual(values = c("Pet shop" = "#54B6B5", "Pet spa" = "#E8BB82", "Total" = "#031B6A"))
     
   }) 
   
@@ -386,7 +433,9 @@ server <- function(input, output) {
         labs(x = "Mes",
              y = "Monto (en soles)") +
         labs(title = "Evolución de ventas mensuales a la fecha", 
-             color = "Línea de negocio")
+             color = "Línea de negocio") +
+        scale_color_manual(values = c("Máximo" = "#54B6B5", "Promedio" = "#E8BB82", "Mes actual" = "#031B6A")) 
+      
     }
     else {
       tabla1 <- df %>%
@@ -428,12 +477,34 @@ server <- function(input, output) {
         aes(x = dia, y = montoAcum, color = categoria) +
         stat_summary(aes(y=montoAcum), fun ="mean", geom="point") +
         stat_summary(aes(y=montoAcum), fun ="mean", geom="line") +
-        labs(x = "Mes",
+        labs(x = "Día",
              y = "Monto (en soles)") +
         labs(title = "Evolución de ventas mensuales a la fecha", 
-             color = "Línea de negocio")
+             color = "Línea de negocio") +
+        scale_color_manual(values = c("Máximo" = "#54B6B5", "Promedio" = "#E8BB82", "Mes actual" = "#031B6A")) 
     }
     
+    
+  }) 
+  
+  output$graph3 <- renderPlot({
+    
+    # Tabla diaria que divide las ventas por linea de negocio
+
+    productos <- df %>%
+      group_by(anio, mes, codItem, descripcion) %>% 
+      summarise(cantidadMensual=n()) %>% 
+      filter(anio == input$Year, mes == input$Mes) %>% 
+      arrange(desc(cantidadMensual)) 
+    
+    productos[1:10,] %>% ggplot() +
+      aes(x = reorder(descripcion, -cantidadMensual), y = cantidadMensual) +
+      geom_bar(stat = "identity", position = "dodge", fill = "#54B6B5") +  
+      geom_text(aes(label = round(cantidadMensual)), vjust = -0.5, size = 4) +
+      labs(x = "Producto o servicio",
+           y = "Cantidad vendida") +
+      labs(title = "Top 10 productos y servicios vendidos en el mes") +
+      scale_x_discrete(labels = function(x) str_wrap(x, width = 10)) 
     
   }) 
   
